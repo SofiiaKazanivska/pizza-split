3d6759b0d173df3c612b0bb191f23345ab6e5b3d
Sofiia <sofiakazzz@gmail.com>
2022-10-13 21:41:56 +0300

    The program  prints the minimum number of pizzas (not zero) so that everyone has an equal number of slices and no slice is left.

--- src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
+++ src/main/java/com/epam/rd/autotasks/pizzasplit/PizzaSplit.java
@@ -1,9 +1,24 @@
 package com.epam.rd.autotasks.pizzasplit;
 
+import java.util.Scanner;
+
 public class PizzaSplit {
     public static void main(String[] args) {
         //Write a program, reading number of people and number of pieces per pizza and then
         //printing minimum number of pizzas to order to split all the pizzas equally and with no remainder
+        Scanner x = new Scanner(System.in);
+        int numbers_of_people = x.nextInt();
+        int pieces_of_pizza = x.nextInt();
+        int pizzas = 1;
+
+       if ((pieces_of_pizza % numbers_of_people != 0)) {
+//            while (pieces_of_pizza % numbers_of_people != 0) {
+           while (pizzas * pieces_of_pizza % numbers_of_people != 0 ) {
+                pizzas++;
+//           pieces_of_pizza += pieces_of_pizza ;
+            }
+        }
+        System.out.println(pizzas);
 
 
 
